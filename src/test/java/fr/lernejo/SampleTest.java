2e53f57bd72bdc8067d5e702d8078cffaa944595
Mael Police <police@et.esiea.fr>
2021-11-30 16:57:35 +0100

    Add test to match 100% coverage

---
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -0,0 +1,38 @@
+package fr.lernejo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SampleTest {
+    private final Sample sample = new Sample();
+
+    @Test
+    void opADD() {
+        int a = 2;
+        int b = 3;
+        int addition = sample.op(Sample.Operation.ADD, a, b);
+        Assertions.assertThat(addition).as("addition of 2 + 3").isEqualTo(5);
+    }
+
+    @Test
+    void opMULT() {
+        int a = 2;
+        int b = 3;
+        int multiplication = sample.op(Sample.Operation.MULT, a, b);
+        Assertions.assertThat(multiplication).as("multiplication of 2 x 3").isEqualTo(6);
+    }
+
+    @Test
+    void factExceptionBy0() {
+        int n = 0;
+        int factor = sample.fact(n);
+        Assertions.assertThat(factor).as("factor of 0!").isEqualTo(1);
+    }
+
+    @Test
+    void fact() {
+        int n = 3;
+        int factor = sample.fact(n);
+        Assertions.assertThat(factor).as("factor of 3!").isEqualTo(6);
+    }
+}
